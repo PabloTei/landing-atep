05fe18d15ab3e27a3d35bf3da71ccb46bcdb9921
PabloTei <pteijeirolopez@gmail.com>
2024-10-27 23:44:30 +0100

    se modifica seccion about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,22 +7,22 @@ import { motion } from "framer-motion";
 const values = [
   {
     name: "Assistance",
-    img: "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "https://res.cloudinary.com/depifliz3/image/upload/v1730068041/ATEP-CONSULTING/laurent-cassagne-F60pSELPwjE-unsplash_md8oxt.jpg",
     text: "At ATEP, we understand that in the world of business, timely assistance can make all the difference. Our expert team is here to offer you advice and support at every phase of your project. From initial implementation to ongoing support, we are committed to being your trusted partner, providing the assistance you need to achieve your goals efficiently and effectively.",
   },
   {
     name: "Trust",
-    img: "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "https://res.cloudinary.com/depifliz3/image/upload/v1730067833/ATEP-CONSULTING/krakenimages-Y5bvRlcCx8k-unsplash_piqndb.jpg",
     text: "Trust is a value that we consider sacred at ATEP. We strive to earn and maintain your trust through honesty, integrity and consistency in our actions. By choosing ATEP, you choose a partner you can trust to deliver reliable solutions and exceptional results. Let's build together a solid relationship based on mutual trust.",
   },
   {
     name: "Excellence",
-    img: "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "https://res.cloudinary.com/depifliz3/image/upload/v1730067833/ATEP-CONSULTING/brad-neathery-nPy0X4xew60-unsplash_omr8ha.jpg",
     text: "Excellence is the compass that guides our operations at ATEP. We constantly seek perfection in each project, from strategic planning to detailed execution. Our commitment to excellence is reflected in the quality of our services and our dedication to exceeding your expectations. At ATEP, we believe that excellence is not an occasional achievement, but a constant habit.",
   },
   {
     name: "Professionality",
-    img: "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    img: "https://res.cloudinary.com/depifliz3/image/upload/v1730067833/ATEP-CONSULTING/john-schnobrich-FlPc9_VocJ4-unsplash_ivtvah.jpg",
     text: "At ATEP, professionalism is the backbone of our approach. Our team of highly trained SAP consultants operates with an exceptional standard of professionalism in every interaction. We are proud to offer world-class solutions with a professional and ethical approach. By choosing ATEP, you choose the experience and professionalism necessary to tackle the most complex business challenges with confidence.",
   },
 ];
@@ -74,9 +74,9 @@ const About = () => {
               <Slider {...settings}>
                 {values.map((value) => (
                   <div key={value.name} className="my-6">
-                    <div className="flex flex-col sm:flex-row gap-5 md:gap-14 p-4 mx-4 rounded-xl relative">
+                    <div className="flex flex-col sm:flex-row gap-5 md:gap-14 p-4 mx-4 rounded-xl relative group">
                       <img
-                        className="block mx-auto h-[300px] w-full sm:w-[200px] object-cover rounded-xl"
+                        className="block mx-auto h-[300px] w-full sm:w-[300px] object-cover rounded-xl filter grayscale group-hover:filter-none transition-all duration-300"
                         src={value.img}
                         alt={value.name}
                       />
