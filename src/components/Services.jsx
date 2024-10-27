6ed064781c0ecf2b4c29fae884124faab96071c5
PabloTei <pteijeirolopez@gmail.com>
2024-10-27 17:46:36 +0100

    se arregla section services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,32 +2,38 @@ import React from "react";
 import { FaCameraRetro } from "react-icons/fa";
 import { FiArrowUpRight } from "react-icons/fi";
 import { SlNote } from "react-icons/sl";
+import {
+  MdInstallDesktop,
+  MdSupportAgent,
+  MdModelTraining,
+} from "react-icons/md";
+import { GrVmMaintenance } from "react-icons/gr";
 
 const servicesData = [
   {
     name: "Installation",
-    icon: <FaCameraRetro className="text-4xl" />,
+    icon: <MdInstallDesktop className="text-5xl" />,
     description:
       "SAP installation experts for an efficient and hassle-free implementation. Your system will be ready ready to operate with our specialized service.",
     aosDelay: "0",
   },
   {
     name: "Support",
-    icon: <FaCameraRetro className="text-4xl" />,
+    icon: <MdSupportAgent className="text-5xl" />,
     description:
       "Support Service: We offer specialized technical assistance to guarantee the optimal functioning of your SAP system. Our team of experts is ready to solve your problems and provide fast and efficient solutions.",
     aosDelay: "300",
   },
   {
     name: "Maintenance",
-    icon: <SlNote className="text-4xl" />,
+    icon: <GrVmMaintenance className="text-5xl" />,
     description:
       "SAP Maintenance: Our maintenance service ensures the continuity and efficiency of your SAP system. We carry out periodic updates, corrections and optimizations to guarantee optimal performance and minimize possible problems.",
     aosDelay: "500",
   },
   {
     name: "Training",
-    icon: <SlNote className="text-4xl" />,
+    icon: <MdModelTraining className="text-5xl" />,
     description:
       "SAP Training: Boost your team's performance with our specialized SAP trainings. We offer programs tailored to your needs, providing key knowledge to maximize your efficiency and make the most of SAP capabilities in your business environment.",
     aosDelay: "700",
@@ -48,7 +54,10 @@ const Services = () => {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
             {servicesData.map((skill) => (
-              <div key={skill.name} className="card space-y-3 sm:space-y-4 p-4">
+              <div
+                key={skill.name}
+                className="card space-y-3 sm:space-y-4 p-4 bg-gray-50 hover:shadow-md transition-shadow duration-300"
+              >
                 <div>{skill.icon}</div>
                 <h1 className="text-lg font-semibold">{skill.name}</h1>
                 <p className="text-gray-600">{skill.description}</p>
